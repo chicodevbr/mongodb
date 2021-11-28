a46731bdad39d53c63976ba8551871d0fd64b739
chicodevbr <chicodevbr@gmail.com>
2021-11-28 14:52:14 -0300

    add mongoose config

---
+++ mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const db = require('./config');
+
+const Product = require('./models/product');
+
+const url = db.url;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
+
+const createProduct = async (req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+};
+
+exports.createProduct = createProduct;
